9545524ee95f79a629edff8b875ca6e88d231773
lucas.dias <lucasadias1988@gmail.com>
2026-07-24 20:18:35 -0300

    Inicio estruturação da arquitetura e configurações do router e npm install.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createPinia } from 'pinia'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+import '@/assets/css/variables.css'
+import '@/assets/css/components.css'
+import '@/assets/css/main.css'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
