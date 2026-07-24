eedd92ba615b291017b856d95f7cf292abd941bc
lucas.dias <lucasadias1988@gmail.com>
2026-07-24 09:18:02 -0300

    Implementação AuthService.

--- backend/src/main/java/com/bookmanager/backend/service/AuthService.java
+++ backend/src/main/java/com/bookmanager/backend/service/AuthService.java
@@ -1,5 +1,84 @@
 package com.bookmanager.backend.service;
 
+import com.bookmanager.backend.configJWT.JwtService;
+import com.bookmanager.backend.dto.LoginRequest;
+import com.bookmanager.backend.dto.LoginResponse;
+import com.bookmanager.backend.dto.RegisterRequest;
+import com.bookmanager.backend.model.User;
+import com.bookmanager.backend.repository.UserRepository;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthService {
-    
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+
+    public AuthService(
+            UserRepository userRepository,
+            PasswordEncoder passwordEncoder,
+            JwtService jwtService,
+            AuthenticationManager authenticationManager
+    ) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+    }
+
+
+    public LoginResponse register(RegisterRequest request) {
+
+        if (userRepository.existsByEmail(request.getEmail())) {
+            throw new RuntimeException("Email já cadastrado");
+        }
+
+
+        User user = new User(
+                request.getName(),
+                request.getEmail(),
+                passwordEncoder.encode(request.getPassword())
+        );
+
+
+        userRepository.save(user);
+
+
+        String token = jwtService.generateToken(user);
+
+        return new LoginResponse(token);
+    }
+
+
+    public LoginResponse login(LoginRequest request) {
+
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+
+
+        User user = userRepository
+                .findByEmail(request.getEmail())
+                .orElseThrow(() ->
+                        new RuntimeException("Usuário não encontrado")
+                );
+
+
+        String token = jwtService.generateToken(user);
+
+        return new LoginResponse(token);
+    }
 }
